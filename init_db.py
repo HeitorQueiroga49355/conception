e58002c7623a622c923f977875d07ccc29d70546
Thiago Morais <you@example.com>
2023-02-04 18:19:27 -0300

    começando a estilizar as paginas

--- init_db.py
+++ init_db.py
@@ -1,13 +1,14 @@
 import mysql.connector
 
+# conn = mysql.connector.connect(user='root',
+#                                 password='',
+#                                 host='localhost',
+#                                 port='3306')
+
 conn = mysql.connector.connect(user='root',
-                                password='',
+                                password='Tms.142226',
                                 host='localhost',
                                 port='3306')
-
-# conn = mysql.connector.connect(user='root',
-#                                 password='Tms.142226',
-#                                 host='127.0.0.1')
 
 cursor = conn.cursor()
 
